31eabe5faeffe17b45e72f9267334bb563788dcd
OzzyBeagle <jakub.jankowski5@gmail.com>
2022-04-25 20:25:16 +0200

    Next task

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,63 @@ public class Main {
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");*/
 
-        int input;
+/*        int input;
 
         System.out.println("Write how many cups of coffee you will need:");
         input = scanner.nextInt();
         System.out.println("For 25 cups of coffee you will need:");
         System.out.println(input * 200 + " ml of water");
         System.out.println(input * 50 + " ml of milk");
-        System.out.println(input * 15 + " g of coffee beans");
+        System.out.println(input * 15 + " g of coffee beans");*/
 
+        int water;
+        int milk;
+        int coffee;
+        int cups;
+        int howMany;
 
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffe machine has:");
+        coffee = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
+        cups = scanner.nextInt();
+
+        int howManyFromWater = water / 200;
+        int howManyFromMilk = milk / 50;
+        int howManyFromCoffee = coffee / 15;
+
+        if(howManyFromWater >= cups && howManyFromMilk >= cups && howManyFromCoffee >= cups) {
+            if(howManyFromWater < howManyFromMilk){
+                if(howManyFromWater < howManyFromCoffee){
+                    howMany = howManyFromWater;
+                } else{
+                    howMany = howManyFromCoffee;
+                }
+            }
+             else if(howManyFromMilk < howManyFromCoffee){
+                howMany = howManyFromMilk;
+            } else {
+                howMany = howManyFromCoffee;
+            }
+
+            if(howMany == cups) {
+                System.out.println("Yes, I can make that amount of coffee");
+            } else {
+                System.out.println("Yes, I can make that amount of coffee (and even " + (howMany - cups) + " more then that)");
+            }
+
+        } else {
+            int tmp = howManyFromWater;
+            if(tmp > howManyFromMilk) {
+                tmp = howManyFromMilk;
+            }
+            if(tmp > howManyFromCoffee) {
+                tmp = howManyFromCoffee;
+            }
+            System.out.println("No, I can make only " + tmp + " cups of coffee");
+        }
     }
 }
